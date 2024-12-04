1b56007251b39d294ec36f4a1de10cb1b25464e5
Sriram Sridharan <s.sriram@yethi.in>
2024-12-04 19:24:30 +0530

    finished lecture 23

--- ice_breaker/ice_breaker.py
+++ ice_breaker/ice_breaker.py
@@ -1,17 +1,19 @@
+from typing import Tuple
+
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from constants import MODEL_OPENAI_GPT_4O_MINI
-from output_parsers import summary_parser
+from output_parsers import summary_parser, Summary
 from third_parties.linkedin import scrape_linkedin_profile
 
 
 # from langchain_ollama import ChatOllama
 
 
-def ice_break_with(name: str):
+def ice_break_with(name: str) -> Tuple[Summary, str]:
     linkedin_profile_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url, mock=True)
     summary_template = """
@@ -29,8 +31,8 @@ def ice_break_with(name: str):
 
     llm = ChatOpenAI(temperature=0, model_name=MODEL_OPENAI_GPT_4O_MINI)
     chain = summary_prompt_template | llm | summary_parser
-    res = chain.invoke(input={"information": linkedin_data})
-    print(res)
+    res:Summary = chain.invoke(input={"information": linkedin_data})
+    return res, linkedin_data.get("profile_pic_url")
 
 
 if __name__ == "__main__":
