ed23097c29ec0c746356967925d51b79909ce7a6
Sriram Sridharan <s.sriram@yethi.in>
2024-12-04 15:47:00 +0530

    finished lecture-22 - Add Pydantic output parser

--- ice_breaker/ice_breaker.py
+++ ice_breaker/ice_breaker.py
@@ -4,6 +4,7 @@ from langchain_openai import ChatOpenAI
 
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from constants import MODEL_OPENAI_GPT_4O_MINI
+from output_parsers import summary_parser
 from third_parties.linkedin import scrape_linkedin_profile
 
 
@@ -12,19 +13,22 @@ from third_parties.linkedin import scrape_linkedin_profile
 
 def ice_break_with(name: str):
     linkedin_profile_url = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url, mock=True)
     summary_template = """
             Given the LinkedIn information {information} about a person, I want you to create:
             1. a short summary
             2. two interesting facts about them
+            
+            \n{format_instructions}
         """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"], template=summary_template,
+        partial_variables={"format_instructions": summary_parser.get_format_instructions()}
     )
 
     llm = ChatOpenAI(temperature=0, model_name=MODEL_OPENAI_GPT_4O_MINI)
-    chain = summary_prompt_template | llm | StrOutputParser()
+    chain = summary_prompt_template | llm | summary_parser
     res = chain.invoke(input={"information": linkedin_data})
     print(res)
 
